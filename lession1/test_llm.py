c7347b11ac9ef09bc82e4258d33a05b454927d21
wenrui <wenrui199231@gamil.com>
2024-07-01 00:14:52 +0800

    所有的百度免费模型

--- lession1/test_llm.py
+++ lession1/test_llm.py
@@ -3,7 +3,9 @@ import os
 from langchain_community.chat_models import QianfanChatEndpoint
 from langchain_core.tracers import ConsoleCallbackHandler
 
+# 免费等模型 ERNIE-Speed-128K ERNIE-Speed-8K ERNIE Speed-AppBuilder ERNIE-Lite-8K
+# ERNIE-Lite-8K-0922 ERNIE-Lite-AppBuilder-8K ERNIE-Tiny-8K Yi-34B-Chat
 BAIDU_LLM = QianfanChatEndpoint(
-    model="ERNIE-Lite-8K"
+    model="ERNIE-Speed-8K"
 )
 BAIDU_LLM.invoke("你是谁",config = {"callbacks": [ConsoleCallbackHandler()]})
