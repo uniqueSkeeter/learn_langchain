891bbe738bf4ee06559a3e8da3ff7d347e38d573
wenrui <wenrui199231@gamil.com>
2024-07-01 00:00:51 +0800

    test

--- lession1/test_llm.py
+++ lession1/test_llm.py
@@ -1,3 +1,9 @@
+import os
 
-print("hello world")
-print("hello wenrui")
+from langchain_community.chat_models import QianfanChatEndpoint
+from langchain_core.tracers import ConsoleCallbackHandler
+
+BAIDU_LLM = QianfanChatEndpoint(
+    model="ERNIE-Lite-8K"
+)
+BAIDU_LLM.invoke("你是谁",config = {"callbacks": [ConsoleCallbackHandler()]})
